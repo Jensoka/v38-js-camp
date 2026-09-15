a3988ed6570cd555346733cf2f7c55cb0a8ed64f
Bachchan Thapa <bachchan.k.t@gmail.com>
2026-09-15 11:10:17 +0200

    feat: introduce const/let and basic types Co-authored-by: Jensoka <Jensoka@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-console.log("Nord Glow");
+/* console.log("Nord Glow");
 console.log("Nord Glow");
 console.log("Nord Glow");
 // jag rörde 3 rader
+
+
+ */
+
+const eventName = "Thapa live";
+let ticketCount = 0;
+
+console.log(eventName);
+console.log(eventName);
+ticketCount = 10;
+console.log(ticketCount);
+const isVip = false;
+console.log(isVip);
